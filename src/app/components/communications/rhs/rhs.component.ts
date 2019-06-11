385db722f7bda2639f125ef4ca264321f2f76bd4
michaelhsladetrng <51245469+michaelhsladetrng@users.noreply.github.com>
2019-06-11 15:02:17 +0000

    next ver of subscription

--- src/app/components/communications/rhs/rhs.component.ts
+++ src/app/components/communications/rhs/rhs.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommunicationsService } from '../communications.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-rhs',
   templateUrl: './rhs.component.html',
   styleUrls: ['./rhs.component.css']
 })
-export class RhsComponent implements OnInit {
+export class RhsComponent implements OnInit, OnDestroy {
 
   message = '';
   message2: string;
+  subscription: Subscription;
   constructor(private service: CommunicationsService) {
-    service.getSubscription()
+  }
+
+  ngOnInit() {
+    this.subscription = this.service.getSubscription()
       .subscribe(newMessage => {
         console.log('Got a new message: ', newMessage);
         this.message2 = newMessage;
       });
   }
 
-  ngOnInit() {
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   getIt() {
