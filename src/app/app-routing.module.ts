9997460cedd95be1ec688590a83fc5ac6bf423e1
michaelhsladetrng <51245469+michaelhsladetrng@users.noreply.github.com>
2019-06-11 13:15:05 +0000

    Added Dashboard, Nav, and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'todo-list',
+    component: TodoListComponent
+  },
+  {
+    path: '**',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
