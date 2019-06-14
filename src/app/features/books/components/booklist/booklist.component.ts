369b38119e70cabd017426122c3803a058ece234
michaelhsladetrng <51245469+michaelhsladetrng@users.noreply.github.com>
2019-06-14 09:06:18 -0400

    Add Book, Models, and setup book list

--- src/app/features/books/components/booklist/booklist.component.ts
+++ src/app/features/books/components/booklist/booklist.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Book } from '../../models';
 
 @Component({
   selector: 'app-booklist',
   templateUrl: './booklist.component.html',
-  styleUrls: ['./booklist.component.css']
+  styleUrls: ['./booklist.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BooklistComponent implements OnInit {
+
+  @Input() items: Book[] = [];
 
   constructor() { }
 
